b798f36bca85332d88ea2820b79587edf24798b6
Matt Johnson <mrjoh3@gmail.com>
2021-06-01 08:55:01 +0000

    add roxygen and switch to qrencoder

--- R/grid.R
+++ R/grid.R
@@ -1,10 +1,31 @@
 
 
+#' @title Add Tracking Footer
+#'
+#' @param gg ggplot object to track
+#' @param qr_content character to encode into QR code
+#' @param color character color of the QR code
+#' @param height_plot integer centimeters
+#' @param height_tracker integer centimeters
+#' @param caption character text to add to footer
+#' @param logo file to add to footer as a logo
+#'
+#' @import ggplot2
+#' @import grid
+#' @import gridExtra
+#' @import qrencoder
+#' @import png
+#' @import RCurl
+#'
+#' @return
+#' @export
+#'
+#' @examples
 ggtrack <- function(gg, qr_content, color = 'black', height_plot = 10, height_tracker = 1.8, caption = NULL, logo = NULL) {
 
+
   # setup QR code
-  qr_matrix <- qrcode::qrcode_gen(paste0(qr_content, ' ', format(Sys.time(), '%Y%m%d-%H%M%S')),
-                                  plotQRcode = FALSE, dataOutput = TRUE)
+  qr_matrix <- qrencoder::qrencode(paste0(qr_content, ' ', format(Sys.time(), '%Y%m%d-%H%M%S')))
 
   qr_matrix[qr_matrix == 1] <- color
   qr_matrix[qr_matrix == 0] <- 'white'
@@ -13,13 +34,13 @@ ggtrack <- function(gg, qr_content, color = 'black', height_plot = 10, height_tr
 
   tracker <- ggplot(mapping = aes(x = 0:1, y = 1)) +
     theme_void() +
-    annotation_custom(qr, xmin = .8, xmax = 1)
+    annotation_custom(qr, xmin = .9, xmax = 1)
 
   # setup logo
   if (!is.null(logo)) {
     lg <- grid::rasterGrob(png::readPNG(RCurl::getURLContent(logo)), just = 'right', height = unit(height_tracker, 'cm'), name = 'logo')
     tracker <- tracker +
-      annotation_custom(lg, xmin = .5, xmax = .8)
+      annotation_custom(lg, xmin = .7, xmax = .9)
   }
 
 
