3aa4592c20d74c84be69a65a9753e78c8bae6aaf
Matt Johnson <mrjoh3@gmail.com>
2021-06-27 19:06:56 +1000

    package file

---
+++ R/ggtrack-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @useDynLib ggtrack, .registration = TRUE
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
