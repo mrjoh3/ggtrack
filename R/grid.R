4d7e02a9131e884d99eaa9edcc5b84ce7ebedafb
Matt Johnson <mrjoh3@gmail.com>
2021-06-30 22:06:44 +1000

    add option to rearrage and size containers

--- R/grid.R
+++ R/grid.R
@@ -3,13 +3,18 @@
 #' @title Add Tracking Footer
 #'
 #' @param gg ggplot object to track
-#' @param qr_content character to encode into QR code
+#' @param qr_content \code{character} content passed to \link[qrencoder]{qrencode}. A time stamp is automatically added plus the current
+#' git commit where available.
 #' @param color character color of the QR code
+#' @param caption \code{character} or \code{grob} to add to footer. Text can be
+#' \code{html} or \code{md} and is passed directly to \link[gridtext]{richtext_grob}
+#' @param logo file to add to footer as a logo
+#' @param order
+#' @param positions
 #' @param height_plot integer centimeters
 #' @param height_tracker integer centimeters
 #' @param interactive logical use plotly for interactivity
-#' @param caption character text to add to footer
-#' @param logo file to add to footer as a logo
+
 #' @param plotly_heights numeric vector of length 2 to fix relative height of plot and tracking banner
 #'
 #' @import ggplot2
@@ -21,22 +26,27 @@
 #' @export
 #'
 #' @examples
-ggtrack <- function(gg, qr_content, color = 'black', height_plot = 10, height_tracker = 1.8, interactive = FALSE, caption = NULL, logo = NULL, plotly_heights = c(.8,.2)) {
+ggtrack <- function(gg, qr_content, color = 'black', caption = NULL, logo = NULL, order = 'CLQ', positions = c(55, 25, 20), height_plot = 10, height_tracker = 1.8, interactive = FALSE, plotly_heights = c(.8,.2)) {
 
-  # build tracker as plot
+  # define size and order of 3 containers
+  pos <- get_positions(order, positions)
+
+  # build tracker as plot, this is the tracker object
   tracker <- ggplot(mapping = aes(x = 0:1, y = 1)) +
     theme_void()
 
-  tracker <- add_qr(tracker, qr_content, color, height_tracker)
+  class(tracker) <- c(class(tracker), 'ggtracker')
+
+  tracker <- add_qr(tracker, qr_content, color, height_tracker, pos)
 
   # setup logo
   if (!is.null(logo)) {
-    tracker <- add_logo(tracker, logo, height_tracker)
+    tracker <- add_logo(tracker, logo, height_tracker, pos)
   }
 
   # setup caption
   if (!is.null(caption)) {
-    tracker <- add_caption(tracker, caption)
+    tracker <- add_caption(tracker, caption, pos)
   }
 
   # style tracker
