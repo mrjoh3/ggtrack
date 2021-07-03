07d7d239c0b3fb9627e5751e469114af2fc74633
Matt Johnson <mrjoh3@gmail.com>
2021-07-03 23:06:53 +1000

    fix add theme docs

--- R/grid.R
+++ R/grid.R
@@ -199,15 +199,15 @@ add_banner <- function(gg, tracker, height_plot = 7) {
 #' @title Modify Tracking Banner Theme
 #'
 #' @param tracker ggtrack tracker object
-#' @param ... options passed to \link[ggplot2]{theme} in order to style the tracker banner.
+#' @param ... options passed to \link[ggplot2]{theme} in order to style the tracker banner
 #'
 #' @return tracker
 #' @export
 #'
 #' @examples
-#'
+#' \dontrun{
 #'   make_tracker() %>% add_theme(plot.background = ggplot2::element_rect(fill = "red", size = 0))
-#'
+#' }
 add_theme <- function(tracker, ...) {
 
   height_tracker <- tracker$height
