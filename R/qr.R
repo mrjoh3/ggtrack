0aeb78957e70105477c7a4f6b92aabde7aa812b9
Jesus M. Castagnetto <jesus@castagnetto.com>
2021-07-04 22:38:40 -0500

    Fix for issues #16 and #17
    - Check if we are inside a dir tracked by git before getting the HEAD hash
    - Make git hash and timestamp optional

--- R/qr.R
+++ R/qr.R
@@ -84,24 +84,30 @@ add_qr.tracker <- function(tracker, qr_content, color = 'black', color_bg = 'whi
 #'
 #' @return matrix
 #'
-make_qr <- function(qr_content, color = 'black', color_bg = 'white') {
+make_qr <- function(qr_content, color = 'black', color_bg = 'white', add_git = TRUE, add_ts = TRUE) {
 
-  g <- Sys.which('git')
+  qr_txt = qr_content
+  
+  if (add_git) {
+    g <- Sys.which('git')
 
-  if (as.character(g) == '') {
-    git <- 'noGit'
-  } else {
-    git_sha <- strtrim(system("git rev-parse HEAD", intern=TRUE), 7)
-    git_url <- system("git remote show origin", intern=TRUE)
-    git_url <- gsub('  Fetch URL: ', '', git_url[grepl('Fetch', git_url)])
-
-    git <- paste0(git_url, ' ', git_sha)
+    if (as.character(g) != '') {
+      # we might have git but not be in a git repo
+      in_git_repo <- system("git rev-parse --git-dir", ignore.stderr = TRUE)
+      if (in_git_repo == 0) {
+        git <- strtrim(system("git rev-parse HEAD", intern=TRUE), 7)
+      } else {
+        git <- ""
+      }
+    }
+    qr_txt = paste(qr_txt, git)
   }
 
+  if (add_ts) {
+    qr_txt = paste(qr_txt, format(Sys.time(), '%Y%m%d-%H%M%S'))
+  }
 
-  qr_matrix <- qrencoder::qrencode(paste(qr_content,
-                                         git,
-                                         format(Sys.time(), ' %Y%m%d-%H%M%S')))
+  qr_matrix <- qrencoder::qrencode(qr_txt)
 
   qr_matrix[qr_matrix == 1] <- color
   qr_matrix[qr_matrix == 0] <- color_bg
