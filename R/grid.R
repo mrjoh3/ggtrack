3ed86f0801dcceb68b6476bf5bf6d475a67acee5
Matt Johnson <mrjoh3@gmail.com>
2021-07-05 23:02:22 +1000

    move theme to separate module

--- R/grid.R
+++ R/grid.R
@@ -198,34 +198,6 @@ add_banner <- function(gg, tracker, height_plot = 7) {
 
 
 
-#' @title Modify Tracking Banner Theme
-#'
-#' @param tracker ggtrack tracker object
-#' @param ... options passed to \link[ggplot2]{theme} in order to style the tracker banner
-#'
-#' @return tracker
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#'   make_tracker() %>% add_theme(plot.background = ggplot2::element_rect(fill = "red", size = 0))
-#' }
-add_theme <- function(tracker, ...) {
-
-  height_tracker <- tracker$height
-  position <- tracker$pos
-  banner <- tracker$track
-
-  tracker$track <- banner + theme(...)
-
-  mtrack <- obj_tracker(tracker, position, height_tracker, 'theme')
-
-  return(mtrack)
-
-}
-
-
-
 #' @title Print Tracker
 #'
 #' @param x ggtrack tracker object
