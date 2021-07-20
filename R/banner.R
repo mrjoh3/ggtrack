9fa36ba96c62b01a7a445b1156d883bcf725bc6c
Matt Johnson <mrjoh3@gmail.com>
2021-07-20 22:34:41 +1000

    trying to widen banner as patchwork aligns content

--- R/banner.R
+++ R/banner.R
@@ -5,8 +5,13 @@
 #' @param gg ggplot object to track
 #' @param tracker ggtrack tracker object
 #' @param height_plot numeric tracker height in cm.
+#' @param banner_layout plot_layout object created by \link[patchwork]{plot_layout}. Only
+#' required where input is a `patchwork` object. Must include a banner height element
+#' that is available from the `ggtrack` object via `$height`. The banner is added to
+#' the patchwork object via `/`.
 #'
 #' @import patchwork
+#' @importFrom ggplot2 margin
 #'
 #' @return tracker
 #' @export
@@ -21,7 +26,7 @@
 #'     add_banner(track)
 #'
 #' }
-add_banner <- function(gg, tracker, height_plot = 7) {
+add_banner <- function(gg, tracker, height_plot = 7, banner_layout = NULL) {
 
   if (missing(gg)) {
     gg <- ggplot() + theme_void()
@@ -31,11 +36,18 @@ add_banner <- function(gg, tracker, height_plot = 7) {
   height_tracker <- tracker$height
 
   tracker$track <- tracker$track +
-    theme(plot.margin=unit(c(.5, 0, .3, 0),"cm"))
+    theme(plot.margin = margin(.5, 0, .3, 0, "cm"),
+          axis.text = element_blank(),
+          axis.title = element_blank(),
+          panel.border = element_blank())
+
+  if (is.null(banner_layout)) {
+    banner_layout <- plot_layout(heights = unit(c(height_plot, height_tracker), 'cm'))
+  }
 
   #gridExtra::grid.arrange(gg, tracker$track, heights = unit(c(height_plot, height_tracker + 1.5 ), "cm"))
-  gg / tracker$track +
-          plot_layout(heights = unit(c(height_plot, height_tracker), 'cm'))
+  (gg) / tracker$track +
+          banner_layout
 
 }
 
